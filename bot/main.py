0a170b3d620fbba11b26baf84fe87013c109fa67
mythseller <77974882+mythseller@users.noreply.github.com>
2022-04-23 12:22:43 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -23,9 +23,11 @@ async def on_message(message):
     # Fetch target channels
     futures_channel = guild.get_channel(951638778160750603)
     options_channel = guild.get_channel(967359150176747621)
+    commentary_channel = guild.get_channel(967358615017115658)
     # Fetch target roles
     futures_role = guild.get_role(953020148623749191)
     options_role = guild.get_role(967361125068972043)
+    commentary_role = guild.get_role(967368279595315200)
 
     if futures_role in message.role_mentions:
         msg = message.content.strip(f"<@&{futures_role.id}>")
@@ -53,9 +55,22 @@ async def on_message(message):
             name=message.author.display_name, icon_url=message.author.avatar_url
         )
         embed.set_thumbnail(url=message.author.avatar_url)
-        noti = await futures_channel.send(msg + " - " + f"<@&{options_role.id}>")
+        noti = await options_channel.send(msg + " - " + f"<@&{options_role.id}>")
         await noti.delete()
         message = await options_channel.send(embed=embed)
+        await message.add_reaction("👍")
+        
+    elif commentary_role in message.role_mentions:
+        msg = message.content.strip(f"<@&{commentary_role.id}>")
+        embed = discord.Embed(title=msg, color=0x9b59b6, timestamp=datetime.now())
+        # embed.add_field(name="Trade Type:", value="Options Play")
+        embed.set_author(
+            name=message.author.display_name, icon_url=message.author.avatar_url
+        )
+        embed.set_thumbnail(url=message.author.avatar_url)
+        noti = await commentary_channel.send(msg + " - " + f"<@&{commentary_role.id}>")
+        await noti.delete()
+        message = await commentary_channel.send(embed=embed)
         await message.add_reaction("👍")
         
 @bot.listen()
